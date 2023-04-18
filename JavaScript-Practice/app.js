5c983e359b0b38697fac074c75d6d03b04bb9d20
Kellis Somerville <ksomerville13r2happrentice.org@Administrators-MacBook-Pro.local>
2023-04-18 11:31:20 -0400

    more content added

--- JavaScript-Practice/app.js
+++ JavaScript-Practice/app.js
@@ -1,45 +1,65 @@
-var greet = "Good Morning";
-var year = 2023;
+// var greet = "Good Morning";
+// var year = 2023;
 
-var yearString = year.toString(); // converts values into a string
-var x = 5; // number datatype
-var y = "C13"; // string
+// var yearString = year.toString(); // converts values into a string
+// var x = 5; // number datatype
+// var y = "C13"; // string
 
-console.log(greet + year); // output: Hello2023
-console.log(x); // output: 5
-console.log(y); // output: C13
+// console.log(greet + year); // output: Hello2023
+// console.log(x); // output: 5
+// console.log(y); // output: C13
 
-var greeting = "Hello, Kellis!";
-console.log(greeting);
+// var greeting = "Hello, Kellis!";
+// console.log(greeting);
 
-var age = 22;
-console.log("My Age is", age)
+// var age = 22;
+// console.log("My Age is", age)
 
-var isStudent = true;
-console.log("Is Kellis an Apprentice?", isStudent);
+// var isStudent = true;
+// console.log("Is Kellis an Apprentice?", isStudent);
 
-var result = null; 
-console.log("The Score Is:", result);
+// var result = null; 
+// console.log("The Score Is:", result);
 
-var Name;
-console.log("Employee Full Name: ", Name);
+// var Name;
+// console.log("Employee Full Name: ", Name);
 
-let firstName = "John";
-let lastName = "Doe";
-let name1 = firstName + " " + lastName
-console.log("Full Name:", name1);
+// let firstName = "John";
+// let lastName = "Doe";
+// let name1 = firstName + " " + lastName
+// console.log("Full Name:", name1);
 
-let num1 = 10; // Number datatype
-let num2 = 5; // Number datatype
+// let num1 = 10; // Number datatype
+// let num2 = 5; // Number datatype
 
-let sum = num1 + num2;
-console.log("Sum:", sum); 
+// let sum = num1 + num2;
+// console.log("Sum:", sum); 
 
-let difference = num1 - num2;
-console.log("Difference:", difference);
+// let difference = num1 - num2;
+// console.log("Difference:", difference);
 
-let product = num1 * num2;
-console.log("Product:", product);
+// let product = num1 * num2;
+// console.log("Product:", product);
 
-let quotient = num1 / num2;
-console.log("Quotient:", quotient);
+// let quotient = num1 / num2;
+// console.log("Quotient:", quotient);
+
+// var age = 18;
+// let isAdult = age >= 18;
+// let canDrink = isAdult && age >= 21;
+// let canVote = isAdult && age >= 18;
+
+
+// console.log("My Age Is", age);
+// console.log("Am I a grown ass man?", isAdult);
+// console.log("Can I sip fr? ", canDrink);
+// console.log("Can I deadass vote?", canVote);
+
+var student2 = {
+    firstName: "John", 
+    lastName: "Doe", 
+    gradeLevel: 12,
+};
+
+console.log(student2);
+
