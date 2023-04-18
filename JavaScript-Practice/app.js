0c005cb9935e30efec224d9833822811628125e2
Kellis Somerville <ksomerville13r2happrentice.org@Administrators-MacBook-Pro.local>
2023-04-18 11:07:04 -0400

    more lines of code

--- JavaScript-Practice/app.js
+++ JavaScript-Practice/app.js
@@ -7,7 +7,39 @@ var y = "C13"; // string
 
 console.log(greet + year); // output: Hello2023
 console.log(x); // output: 5
-console.log(y); // output: "C13"
+console.log(y); // output: C13
 
 var greeting = "Hello, Kellis!";
 console.log(greeting);
+
+var age = 22;
+console.log("My Age is", age)
+
+var isStudent = true;
+console.log("Is Kellis an Apprentice?", isStudent);
+
+var result = null; 
+console.log("The Score Is:", result);
+
+var Name;
+console.log("Employee Full Name: ", Name);
+
+let firstName = "John";
+let lastName = "Doe";
+let name1 = firstName + " " + lastName
+console.log("Full Name:", name1);
+
+let num1 = 10; // Number datatype
+let num2 = 5; // Number datatype
+
+let sum = num1 + num2;
+console.log("Sum:", sum); 
+
+let difference = num1 - num2;
+console.log("Difference:", difference);
+
+let product = num1 * num2;
+console.log("Product:", product);
+
+let quotient = num1 / num2;
+console.log("Quotient:", quotient);
