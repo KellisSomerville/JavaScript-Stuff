760460edaa74ede273ed482ad36088fc897a9631
Kellis Somerville <ksomerville13r2happrentice.org@Administrators-MacBook-Pro.local>
2023-04-18 15:04:53 -0400

    finished 1/3 async problems

--- JavaScript-ASYNC/async.js
+++ JavaScript-ASYNC/async.js
@@ -1,6 +1,11 @@
-// Removes Spaces In A String
+// Function that removes spaces in a string
 
-// Reuturns A Boolean If A Number Is Divisible By 10
+// Function that reuturns a boolean if a number is divisible by 10
 
+var randomNum = 67; //Variable can be any random number
+function quotientOf10 (randomNum) { 
+    return Boolean(randomNum % 10 == 0); // Boolean checks if randomNum value can be divisble by 10
+}
+    console.log(quotientOf10(randomNum)); // logs the quiotentof10 function based on Boolean's result
 
-// Returns The Number Of Vowels In A String
+// Function that returns the number of vowels in a string
