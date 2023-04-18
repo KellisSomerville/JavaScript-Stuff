a8910e156a2dfd2ed48089977db215e4679ae94a
Kellis Somerville <ksomerville13r2happrentice.org@Administrators-MacBook-Pro.local>
2023-04-18 10:32:58 -0400

    added practice files

---
+++ JavaScript-Practice/app.js
@@ -0,0 +1,13 @@
+var greet = "Good Morning";
+var year = 2023;
+
+var yearString = year.toString(); // converts values into a string
+var x = 5; // number datatype
+var y = "C13"; // string
+
+console.log(greet + year); // output: Hello2023
+console.log(x); // output: 5
+console.log(y); // output: "C13"
+
+var greeting = "Hello, Kellis!";
+console.log(greeting);
