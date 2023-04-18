ef72319f49c472bddfa4514eadc8c848c16b3585
Kellis Somerville <ksomerville13r2happrentice.org@Administrators-MacBook-Pro.local>
2023-04-18 11:49:00 -0400

    understanding js more

--- JavaScript-Practice/app.js
+++ JavaScript-Practice/app.js
@@ -18,7 +18,7 @@
 // var isStudent = true;
 // console.log("Is Kellis an Apprentice?", isStudent);
 
-// var result = null; 
+// var result = null;
 // console.log("The Score Is:", result);
 
 // var Name;
@@ -33,7 +33,7 @@
 // let num2 = 5; // Number datatype
 
 // let sum = num1 + num2;
-// console.log("Sum:", sum); 
+// console.log("Sum:", sum);
 
 // let difference = num1 - num2;
 // console.log("Difference:", difference);
@@ -49,17 +49,36 @@
 // let canDrink = isAdult && age >= 21;
 // let canVote = isAdult && age >= 18;
 
-
 // console.log("My Age Is", age);
 // console.log("Am I a grown ass man?", isAdult);
 // console.log("Can I sip fr? ", canDrink);
 // console.log("Can I deadass vote?", canVote);
 
 var student2 = {
-    firstName: "John", 
-    lastName: "Doe", 
-    gradeLevel: 12,
+  firstName: "John",
+  lastName: "Doe",
+  gradeLevel: 12,
+};
+
+var student3 = {
+  firstName: "Kellis",
+  lastName: "Somerville",
+  gradeLevel: 11,
 };
 
 console.log(student2);
+console.log(student3);
 
+alert("Hello, mf!");
+var visitorName = prompt("What is your name, mf?");
+var visitorFavoriteAnime = prompt("What is your favorite anime. mf?");
+
+document.write(
+  "Hello " +
+    visitorName +
+    "!" + " " + 
+    visitorFavoriteAnime +
+    " is a really good anime!"
+);
+
+console.log("Program donezo");
