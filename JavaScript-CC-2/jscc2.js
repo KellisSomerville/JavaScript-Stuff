288c688094a189729512972b6e71469e80e8ae2f
Kellis Somerville <ksomerville13r2happrentice.org@Administrators-MacBook-Pro.local>
2023-04-25 13:51:52 -0400

    finished js coding challenge 2

--- JavaScript-CC-2/jscc2.js
+++ JavaScript-CC-2/jscc2.js
@@ -3,8 +3,8 @@
 let randomNumbsArray = [1, -9, 3, -7, -4, 2, -3, 8, -6]; // Random array of numbers
 let negativeNumbsRemoved = randomNumbsArray.filter(removeNegativeNumbs); // Applies the function by filtering out numbers greater than 0
 
-function removeNegativeNumbs(numbs) {
-    return numbs > 0; // Returns numbers in the array that are greater than 0
+function removeNegativeNumbs(numbers) {
+    return numbers > 0; // Returns numbers in the array that are greater than 0
 }
 
 console.log(randomNumbsArray); // Logs the original array
