d1930adf47043b5916a231bd9790a2ed12b8e7a0
Kellis Somerville <ksomerville13r2happrentice.org@AdminisatorsMBP.lan>
2023-05-01 11:42:46 -0400

    finished js gi 2

--- JavaScript-GI/jsgi2.js
+++ JavaScript-GI/jsgi2.js
@@ -1,15 +1,34 @@
-// // VERY EASY
-// let numb1 = 5;
-// let numb2 = 10;
+// VERY EASY
+let numb1 = 5;
+let numb2 = 10;
 
-// function min (numb1, numb2) {
-//     return Math.min(numb1, numb2); // Returns minimum from numb1 and num2 values
-// }
+function min (numb1, numb2) {
+    return Math.min(numb1, numb2); // Returns minimum from numb1 and num2 values
+}
 
-// console.log(min (numb1, numb2));
+console.log(min (numb1, numb2));
 
 // EASY
+let student1 = ["Gon", "Freecss", 14];
+let student2 = ["Killua", "Zoldyck", 14];
+let student3 = ["Kurapika", "Kurta", 17];
 
+console.log(student1);
+console.log(student2);
+console.log(student3);
+console.log(
+  "Hello, my name is" +
+    " " +
+    student2[0] +
+    " " +
+    student2[1] +
+    " " +
+    "and I'm" +
+    " " +
+    student2[2] +
+    " " +
+    "years old."
+);
 
 // MEDIUM
 let insertMonthNum = prompt("Type A Number From 1-12 To Get The Month Back");
@@ -60,3 +79,16 @@ if ((insertMonthNum == 1)) {
     "Month" + " " + insertMonthNum + " " + "of every year is December."
   );
 }
+
+// HARD
+let tomWeight = 8;
+let tomHeight = 9;
+let jerryWeight = 45;
+let jerryHeight = 10;
+
+let tomBmi = tomWeight / tomHeight ** 2;
+let jerryBmi = jerryWeight / jerryHeight ** 2;
+
+let higherBmi = Boolean(tomBmi > jerryBmi);
+
+console.log("Is Tom's BMI higher than Jerry's BMI?" + " " + higherBmi);
